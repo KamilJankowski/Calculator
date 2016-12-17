505e3bdb3a331a6faf082add9737f1627334ec45
Kamil <kjankowski85@gmail.com>
2016-12-17 21:44:23 +0000

    ...

--- ConsoleApplication2/Program.cs
+++ ConsoleApplication2/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome");
+            Console.WriteLine("Welcome...");
             Console.ReadKey();
         }
     }
