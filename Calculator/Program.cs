f64c75c0a81127cdccad27907c1f60f129023ab5
Kamil <kjankowski85@gmail.com>
2016-12-28 19:53:01 +0000

    class operation (+,-,*,/) and result of operations

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -8,31 +8,54 @@ namespace Calculator
 {
     class first_val
     {
+        private int number;
+
         public void get_val()
         {
-            Console.WriteLine("Please enter first value: ");
+            Console.WriteLine("Please enter value: ");
         }
 
-        public void read_val()
+        public int read_val(int val)
         {
-            string number;
-            number = Console.ReadLine();
-            Console.WriteLine("Your fisrt value is {0} ", number);
+            number = Convert.ToInt32(Console.ReadLine());
 
+            val = number;
+
+            return val;
+        }
+    }
+
+    class operation
+    {
+        public int add(int a,int b)
+        {
+            return a + b;
         }
 
+        public int substract(int a, int b)
+        {
+            return a - b;
+        }
 
-        
+        public int multiple(int a, int b)
+        {
+            return a * b;
+        }
 
-
+        public int devide(int a, int b)
+        {
+            return a / b;
+        }
     }
+
 
 
     class Program
     {
         static void Main(string[] args)
         {
-
+            int a = 0;
+            
             //welcome string declartion
             string welcome = "Welcome to Simple Calculator - created by Kamil Jankowski";
 
@@ -56,9 +79,28 @@ namespace Calculator
 
 
             first_val fval = new first_val();
-            fval.get_val();
-            fval.read_val();
 
+            fval.get_val();
+                        
+            int b = fval.read_val(a);
+
+            Console.WriteLine("Return: {0}", b);
+            fval.get_val();
+            int c = fval.read_val(a);
+
+            Console.WriteLine("Return: {0}", c);
+
+            operation o = new operation();
+            int ra = o.add(b, c);
+            int rs = o.substract(b, c);
+            int rm = o.multiple(b, c);
+            int rd = o.devide(b, c);
+
+
+            Console.WriteLine("Operation ADD Return: {0}", ra);
+            Console.WriteLine("Operation SUBSTARCT: {0}", rs);
+            Console.WriteLine("Operation MULTIPLE: {0}", rm);
+            Console.WriteLine("Operation DEVIDE: {0}", rd);
 
             Console.ReadKey();
         }
