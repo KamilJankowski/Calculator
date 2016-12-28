c98ca65f3299f4997c3bd9db3dac46e6c2844b6c
Kamil <kjankowski85@gmail.com>
2016-12-28 00:12:49 +0000

    class first_val

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -6,11 +6,33 @@ using System.Threading.Tasks;
 
 namespace Calculator
 {
+    class first_val
+    {
+        public void get_val()
+        {
+            Console.WriteLine("Please enter first value: ");
+        }
+
+        public void read_val()
+        {
+            string number;
+            number = Console.ReadLine();
+            Console.WriteLine("Your fisrt value is {0} ", number);
+
+        }
+
+
+        
+
+
+    }
+
+
     class Program
     {
         static void Main(string[] args)
         {
-            
+
             //welcome string declartion
             string welcome = "Welcome to Simple Calculator - created by Kamil Jankowski";
 
@@ -22,27 +44,24 @@ namespace Calculator
 
             Console.WriteLine("");
             Console.WriteLine(welcome);
-           
+
 
             //display character '-' to separate welcome and program content    
-            for(int i = 0; i<welcome.Length; i++)
+            for (int i = 0; i < welcome.Length; i++)
             {
                 Console.Write("-");
             }
             Console.WriteLine("");
             Console.WriteLine("");
 
-            Console.WriteLine("Please enter first value: ");
 
-            //read input ???????
-
-            string number;
-            number = Console.ReadLine();
-            Console.WriteLine("Your fisrt value is {0} ", number);
-            
+            first_val fval = new first_val();
+            fval.get_val();
+            fval.read_val();
 
 
             Console.ReadKey();
         }
     }
+    
 }
