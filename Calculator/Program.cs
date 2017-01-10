d5fec829b8ea8de0fee0d11fa98795be093f01d8
Kamil <kjankowski85@gmail.com>
2017-01-10 22:17:13 +0000

    if and while loop to avoid wrong oper input; if and while loop to avoid wrong choice to contine operation  and

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -9,41 +9,39 @@ namespace Calculator
     //class value
     class first_val
     {
-        private double number;
+        public string str;
+
 
         //ask about value
         public void get_val()
         {
             Console.WriteLine("Please enter value: ");
+            str = Console.ReadLine();
         }
 
         //read value from output and convert string to int32
         public double read_val(double val)
         {
-
-            number = Convert.ToDouble(Console.ReadLine());
-
-            val = number;
-
             return val;
         }
+
     }
 
     //operations like add, substract, multiple, divide
     class operation
     {
         private double ab;
-        public double add(double a,double b)
+        public double add(double a, double b)
         {
-             ab = a + b;
+            ab = a + b;
             return ab;
-            
+
         }
 
         public double substract(double a, double b)
         {
             ab = a - b;
-            return  ab;
+            return ab;
         }
 
         public double multiple(double a, double b)
@@ -55,14 +53,14 @@ namespace Calculator
         public double divide(double a, double b)
         {
             ab = a / b;
-            return  ab;
+            return ab;
         }
 
         public double new_add(double b)
         {
             ab = ab + b;
             return ab;
-            
+
         }
 
         public double new_substract(double b)
@@ -88,22 +86,22 @@ namespace Calculator
 
     }
 
-   
+
 
 
 
     class Program
     {
-        
+
         static void Main(string[] args)
         {
-            double a = 0;
-             
+
+
             //welcome string declaration
             string welcome = "Welcome to Simple Calculator - created by Kamil Jankowski";
 
             //display character '-' to separate welcome and program content  
-            for (double i = 0; i < welcome.Length; i++)
+            for (int i = 0; i < welcome.Length; i++)
             {
                 Console.Write("-");
             }
@@ -113,7 +111,7 @@ namespace Calculator
 
 
             //display character '-' to separate welcome and program content    
-            for (double i = 0; i < welcome.Length; i++)
+            for (int i = 0; i < welcome.Length; i++)
             {
                 Console.Write("-");
             }
@@ -121,163 +119,267 @@ namespace Calculator
             Console.WriteLine("");
 
             //set value
-            first_val fval = new first_val();
-            fval.get_val();
-            double b = fval.read_val(a);
-
-            Console.WriteLine("Return: {0}", b);
-
-            //display operation options
-            Console.WriteLine("Select operation from the following options: ");
-            Console.WriteLine("1) ADD ");
-            Console.WriteLine("2) SUBSTRACT ");
-            Console.WriteLine("3) MULTIPLE ");
-            Console.WriteLine("4) DEVIDE ");
-            char oper = Convert.ToChar(Console.ReadLine());
-
-            //set another value
-            fval.get_val();
-            double c = fval.read_val(a);
-
-            Console.WriteLine("Return: {0}", c);
-
+            double num, b, c, r, val;
             operation o = new operation();
 
-            double r;
+            first_val fval = new first_val();
+            fval.get_val();
 
-            //select and run operation
-            switch (oper)
+            //test and read value 
+
+            while (!(double.TryParse(fval.str, out num)))
             {
-                case '1':
-                     r = o.add(b, c);
-                    Console.WriteLine("Operation ADD Return: {0}", r);
-                    break;
-                case '2':
-                     r = o.substract(b, c);
-                    Console.WriteLine("Operation SUBSTRACT: {0}", r);
-                    break;
-                case '3':
-                     r = o.multiple(b, c);
-                    Console.WriteLine("Operation MULTIPLE: {0}", r);
-                    break;
-                case '4':
-                     r = o.divide(b, c);
-                    Console.WriteLine("Operation DIVIDE: {0}", r);
-                    break;
+                Console.WriteLine("This is not a number. Please try again.");
+                fval.get_val();
+
             }
-
-            Console.WriteLine("Do you want to contine? (y/n): ");
-            char oper_again = Convert.ToChar(Console.ReadLine());
-
-           //if true then continue to ask about operation and value
-            while (oper_again == 'y')
+            if (double.TryParse(fval.str, out num))
             {
-                    //display operation options
+                Console.WriteLine("This is a number.");
+                val = Convert.ToDouble(fval.str);
+                b = fval.read_val(val);
+                Console.WriteLine("Return: {0}", b);
+                //display operation options
+                Console.WriteLine("Select operation from the following options: ");
+                Console.WriteLine("1) ADD ");
+                Console.WriteLine("2) SUBSTRACT ");
+                Console.WriteLine("3) MULTIPLE ");
+                Console.WriteLine("4) DEVIDE ");
+                char oper = Convert.ToChar(Console.ReadLine());
+
+                if ((oper == '1') || (oper == '2') || (oper == '3') || (oper == '4'))
+                {
+                    Console.WriteLine("Good choice");
+                }
+
+                while ((oper != '1') && (oper != '2') && (oper != '3') && (oper != '4'))
+                {
+
+                    Console.WriteLine("No operation. Please select operation from 1 - 4.");
+                    Console.WriteLine();
                     Console.WriteLine("Select operation from the following options: ");
                     Console.WriteLine("1) ADD ");
                     Console.WriteLine("2) SUBSTRACT ");
                     Console.WriteLine("3) MULTIPLE ");
-                    Console.WriteLine("4) DIVIDE ");
+                    Console.WriteLine("4) DEVIDE ");
+
                     oper = Convert.ToChar(Console.ReadLine());
-                    //select and run operation
+                    if ((oper == '1') || (oper == '2') || (oper == '3') || (oper == '4'))
+                    {
+                        Console.WriteLine("Good");
+                    }
+
+                }
+
+
+
+                //set another value
+                fval.get_val();
+                while (!(double.TryParse(fval.str, out num)))
+                {
+                    Console.WriteLine("This is not a number. Please try again.");
+                    fval.get_val();
+
+                }
+                if (double.TryParse(fval.str, out num))
+                {
+                    Console.WriteLine("This is a number.");
+                    val = Convert.ToDouble(fval.str);
+                    c = fval.read_val(val);
+                    Console.WriteLine("Return: {0}", c);
+
+
+
                     switch (oper)
                     {
                         case '1':
-                            fval.get_val();
-                             c = fval.read_val(a);
-                             r = o.new_add(c);
+                            r = o.add(b, c);
                             Console.WriteLine("Operation ADD Return: {0}", r);
                             break;
                         case '2':
-                            fval.get_val();
-                            c = fval.read_val(a);
-                            r = o.new_substract(c);
+                            r = o.substract(b, c);
                             Console.WriteLine("Operation SUBSTRACT: {0}", r);
                             break;
                         case '3':
-                            fval.get_val();
-                            c = fval.read_val(a);
-                             r= o.new_multiple(c);
+                            r = o.multiple(b, c);
                             Console.WriteLine("Operation MULTIPLE: {0}", r);
                             break;
                         case '4':
-                            fval.get_val();
-                            c = fval.read_val(a);
-                             r = o.new_divide(c);
+                            r = o.divide(b, c);
                             Console.WriteLine("Operation DIVIDE: {0}", r);
                             break;
-                    }
-                    Console.WriteLine("Do you want to contine? (y/n): ");
-                    oper_again = Convert.ToChar(Console.ReadLine());
-                    
-                
-            }
-            //terminate program
-            if (oper_again == 'n')
-            {
-                Console.WriteLine("Thank you. God Bye.");
-            }
-            while ((oper_again != 'n') && (oper_again != 'y'))
-            {
-                Console.WriteLine("Wrong data. Please provide correct data (0-9).");
-                Console.WriteLine("Do you want to contine? (y/n): ");
-                oper_again = Convert.ToChar(Console.ReadLine());
 
-                //if true then continue to ask about operation and value
-                while (oper_again == 'y')
-                {
-                    //display operation options
-                    Console.WriteLine("Select operation from the following options: ");
-                    Console.WriteLine("1) ADD ");
-                    Console.WriteLine("2) SUBSTRACT ");
-                    Console.WriteLine("3) MULTIPLE ");
-                    Console.WriteLine("4) DIVIDE ");
-                    oper = Convert.ToChar(Console.ReadLine());
-                    //select and run operation
-                    switch (oper)
+                    }
+
+
+
+                    Console.WriteLine("Do you want to contine? (y/n): ");
+                    char oper_again = Convert.ToChar(Console.ReadLine());
+
+
+
+
+                    //if true then continue to ask about operation and value
+                    while (oper_again == 'y')
                     {
-                        case '1':
-                            fval.get_val();
-                            c = fval.read_val(a);
-                            r = o.new_add(c);
-                            Console.WriteLine("Operation ADD Return: {0}", r);
-                            break;
-                        case '2':
-                            fval.get_val();
-                            c = fval.read_val(a);
-                            r = o.new_substract(c);
-                            Console.WriteLine("Operation SUBSTRACT: {0}", r);
-                            break;
-                        case '3':
-                            fval.get_val();
-                            c = fval.read_val(a);
-                            r = o.new_multiple(c);
-                            Console.WriteLine("Operation MULTIPLE: {0}", r);
-                            break;
-                        case '4':
-                            fval.get_val();
-                            c = fval.read_val(a);
-                            r = o.new_divide(c);
-                            Console.WriteLine("Operation DIVIDE: {0}", r);
-                            break;
+
+                        //display operation options
+                        Console.WriteLine("Select operation from the following options: ");
+                        Console.WriteLine("1) ADD ");
+                        Console.WriteLine("2) SUBSTRACT ");
+                        Console.WriteLine("3) MULTIPLE ");
+                        Console.WriteLine("4) DIVIDE ");
+                        oper = Convert.ToChar(Console.ReadLine());
+
+                        if ((oper == '1') || (oper == '2') || (oper == '3') || (oper == '4'))
+                        {
+                            Console.WriteLine("Good choice");
+                        }
+
+                        while ((oper != '1') && (oper != '2') && (oper != '3') && (oper != '4'))
+                        {
+
+                            Console.WriteLine("No operation. Please select operation from 1 - 4.");
+                            Console.WriteLine();
+                            Console.WriteLine("Select operation from the following options: ");
+                            Console.WriteLine("1) ADD ");
+                            Console.WriteLine("2) SUBSTRACT ");
+                            Console.WriteLine("3) MULTIPLE ");
+                            Console.WriteLine("4) DEVIDE ");
+
+                            oper = Convert.ToChar(Console.ReadLine());
+                            if ((oper == '1') || (oper == '2') || (oper == '3') || (oper == '4'))
+                            {
+                                Console.WriteLine("Good");
+                            }
+
+                        }
+                        //select and run operation
+                        switch (oper)
+                        {
+                            case '1':
+                                fval.get_val();
+                                c = fval.read_val(val);
+                                r = o.new_add(c);
+                                Console.WriteLine("Operation ADD Return: {0}", r);
+                                break;
+                            case '2':
+                                fval.get_val();
+                                c = fval.read_val(val);
+                                r = o.new_substract(c);
+                                Console.WriteLine("Operation SUBSTRACT: {0}", r);
+                                break;
+                            case '3':
+                                fval.get_val();
+                                c = fval.read_val(val);
+                                r = o.new_multiple(c);
+                                Console.WriteLine("Operation MULTIPLE: {0}", r);
+                                break;
+                            case '4':
+                                fval.get_val();
+                                c = fval.read_val(val);
+                                r = o.new_divide(c);
+                                Console.WriteLine("Operation DIVIDE: {0}", r);
+                                break;
+                        }
+                        Console.WriteLine("Do you want to contine? (y/n): ");
+                        oper_again = Convert.ToChar(Console.ReadLine());
+
+
                     }
-                    Console.WriteLine("Do you want to contine? (y/n): ");
-                    oper_again = Convert.ToChar(Console.ReadLine());
+                    //terminate program
+                    if (oper_again == 'n')
+                    {
+                        Console.WriteLine("Thank you. God Bye.");
+                    }
+                    while ((oper_again != 'n') && (oper_again != 'y'))
+                    {
+                        Console.WriteLine("Wrong data. Please provide correct data (0-9).");
+                        Console.WriteLine("Do you want to contine? (y/n): ");
+                        oper_again = Convert.ToChar(Console.ReadLine());
+                    }
+                    //if true then continue to ask about operation and value
+                    while (oper_again == 'y')
+                    {
+                        //display operation options
+                        Console.WriteLine("Select operation from the following options: ");
+                        Console.WriteLine("1) ADD ");
+                        Console.WriteLine("2) SUBSTRACT ");
+                        Console.WriteLine("3) MULTIPLE ");
+                        Console.WriteLine("4) DIVIDE ");
+                        oper = Convert.ToChar(Console.ReadLine());
+
+                        if ((oper == '1') || (oper == '2') || (oper == '3') || (oper == '4'))
+                        {
+                            Console.WriteLine("Good choice");
+                        }
+
+                        while ((oper != '1') && (oper != '2') && (oper != '3') && (oper != '4'))
+                        {
+
+                            Console.WriteLine("No operation. Please select operation from 1 - 4.");
+                            Console.WriteLine();
+                            Console.WriteLine("Select operation from the following options: ");
+                            Console.WriteLine("1) ADD ");
+                            Console.WriteLine("2) SUBSTRACT ");
+                            Console.WriteLine("3) MULTIPLE ");
+                            Console.WriteLine("4) DEVIDE ");
+
+                            oper = Convert.ToChar(Console.ReadLine());
+                            if ((oper == '1') || (oper == '2') || (oper == '3') || (oper == '4'))
+                            {
+                                Console.WriteLine("Good");
+                            }
+
+                        }
+                        //select and run operation
+                        switch (oper)
+                        {
+                            case '1':
+                                fval.get_val();
+                                c = fval.read_val(val);
+                                r = o.new_add(c);
+                                Console.WriteLine("Operation ADD Return: {0}", r);
+                                break;
+                            case '2':
+                                fval.get_val();
+                                c = fval.read_val(val);
+                                r = o.new_substract(c);
+                                Console.WriteLine("Operation SUBSTRACT: {0}", r);
+                                break;
+                            case '3':
+                                fval.get_val();
+                                c = fval.read_val(val);
+                                r = o.new_multiple(c);
+                                Console.WriteLine("Operation MULTIPLE: {0}", r);
+                                break;
+                            case '4':
+                                fval.get_val();
+                                c = fval.read_val(val);
+                                r = o.new_divide(c);
+                                Console.WriteLine("Operation DIVIDE: {0}", r);
+                                break;
+                        }
+                        Console.WriteLine("Do you want to contine? (y/n): ");
+                        oper_again = Convert.ToChar(Console.ReadLine());
 
 
+                    }
+
+                    //terminate program
+                    if (oper_again == 'n')
+                    {
+                        Console.WriteLine("Thank you. God Bye.");
+                    }
                 }
-                //terminate program
-                if (oper_again == 'n')
-                {
-                    Console.WriteLine("Thank you. God Bye.");
-                }
+
+
             }
 
-
+                    Console.ReadKey();
                 
+            }
 
-            Console.ReadKey();
         }
     }
-    
-}
+
